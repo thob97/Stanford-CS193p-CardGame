20ce48edfe0d71838a2404d80feaa8e9a574b239
Thore Brehmer <thoreservice@icloud.com>
2023-09-24 21:35:34 +0200

    Initial Commit

---
+++ StanfordCardGame/StanfordCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordCardGameApp.swift
+//  StanfordCardGame
+//
+//  Created by Thore Brehmer on 24.09.23.
+//
+
+import SwiftUI
+
+@main
+struct StanfordCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
